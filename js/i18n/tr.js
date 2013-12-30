c28ba3c97e795d1fd5a9e826220457b673f2e6fd
abdomin25 <abdomin25@gmail.com>
2013-12-30 20:38:41 +0200

    Create tr.js
    Turkish language pack

---
+++ js/i18n/tr.js
@@ -0,0 +1,21 @@
+// Turkish
+plupload.addI18n({
+    'Select files' : 'Dosya Seç',
+    'Add files to the upload queue and click the start button.' : 'Dosyaları yükleme sırasına koy ve başlat tuşuna bas',
+    'Filename' : 'Dosya ismi',
+    'Status' : 'Durum',
+    'Size' : 'Boyutu',
+    'Add files' : 'Dosya Ekle',
+    'Stop current upload' : 'Yüklemeyi durdur',
+    'Start uploading queue' : 'Yüklemeyi başlat',
+    'Uploaded %d/%d files': 'Yüklenmiş %d / %d dosyalar',
+    'N/A' : 'N/D',
+    'Drag files here.' : 'Dosyaları buraya sürükleyin.',
+    'File extension error.': 'Desteklenmeyen dosya.',
+    'File size error.': 'Dosyanız yüklemek için çok büyük.',
+    'Init error.': 'Hata.',
+    'HTTP Error.': Hata HTTP.',
+    'Security error.': 'Güvenlik hatası.',
+    'Generic error.': 'Genel hata lütfen tekrar deneyiniz.',
+    'IO error.': 'Dosya okuma hatası lütfen tekrar deneyiniz.'
+});
